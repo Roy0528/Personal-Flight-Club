0cf826dd19f4f2490622356131ae5fb03b639838
Roy Wu <roy32011@gmail.com>
2023-12-26 16:02:29 +0800

    Use Sheety to read and write data to the Google

--- modules/data_manager.py
+++ modules/data_manager.py
@@ -1,3 +1,26 @@
+import requests
+
+SHEETY_ENDPOINT = "https://api.sheety.co/d12d1797c31d3f8b7c2e2183d87c56c0/flightDeals/prices"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(url=f"{SHEETY_ENDPOINT}/{city["id"]}",
+                                    json=new_data)
+            print(response.text)
--- modules/flight_search.py
+++ modules/flight_search.py
@@ -1,3 +1,12 @@
+import requests
+import os
+
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/v2"
+TEQUILA_API_KEY = os.getenv("TEQUILA")
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        code = "TESTING"
+        return code
